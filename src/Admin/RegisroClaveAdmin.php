d64ecd6a00c37bbbd38e11d84e1c2bc0f765b3fc
javierviniegra <javierviniegra@hotmail.com>
2021-10-09 19:19:38 -0500

    registroclave

--- src/Admin/RegisroClaveAdmin.php
+++ src/Admin/RegisroClaveAdmin.php
@@ -58,4 +58,27 @@ final class RegisroClaveAdmin extends AbstractAdmin
             ->add('updated_at')
             ;
     }
+
+    public function getNewInstance()
+    {
+        $object = parent::getNewInstance();
+
+        //$object->setDefaults();
+
+        $object->setClave($this->randomAlfanumerico(8));
+
+        return $object;
+    }
+
+    private function randomAlfanumerico($longitud)
+    {
+        $key = '';
+         $keys = array_merge(range(0, 9), range('a', 'z'));
+
+        for ($i = 0; $i < $longitud; $i++) {
+            $key .= $keys[array_rand($keys)];
+        }
+
+        return $key;
+    }
 }
